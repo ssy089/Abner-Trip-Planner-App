8548cc1e8c3813874929b63eb9da8876eb1305dc
ssy089 <ssy089@my.utsa.edu>
2021-08-27 23:58:03 -0500

    Add styles

---
+++ src/client/index.js
@@ -0,0 +1,7 @@
+import './styles/base.scss';
+import './styles/footer.scss';
+import './styles/form.scss';
+import './styles/header.scss';
+import './styles/main.scss';
+
+import './media/globe-3411506_1920.jpg';

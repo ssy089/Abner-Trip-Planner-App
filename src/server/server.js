c54fa4b2a49b58c238dc6ddf6e98d2abf2c32c6d
ssy089 <ssy089@my.utsa.edu>
2021-09-08 23:51:27 -0500

    docs: Add comments to server code

--- src/server/server.js
+++ src/server/server.js
@@ -1,3 +1,4 @@
+/* Import the functions that are used to sort the list of trips. */
 const mergeTripArrays = require('./sortTrips').mergeTripArrays;
 const tripsIterativeMergeSort = require('./sortTrips').tripsIterativeMergeSort;
 
@@ -12,18 +13,19 @@ const express = require('express');        //Used for creating the server
 const fetch = require('node-fetch');       //Used for implementing the Fetch API
 const port = 8081;                         //Port number for the server
 
+/* Set the API keys for the GeoNames API, Weatherbit Weather API, and Pixabay API. */
 const GEONAMES_USERNAME = process.env.GEONAMES_USERNAME;
 const PIXABAY_API_KEY = process.env.PIXABAY_API_KEY;
 const WEATHERBIT_API_KEY = process.env.WEATHERBIT_API_KEY;
 
+/* Set the base URLs that will be used to fetch data from the APIs. */
 const geonamesBaseURL = `http:\/\/api.geonames.org\/search?username=${GEONAMES_USERNAME}&type=json`;
 const pixabayBaseURL = `https:\/\/pixabay.com\/api\/?key=${PIXABAY_API_KEY}&safesearch=true&image_type=photo`;
-const weatherbit_current_baseURL = `http:\/\/api.weatherbit.io\/v2.0\/current?key=${WEATHERBIT_API_KEY}`;
 const weatherbit_forecast_baseURL = `http:\/\/api.weatherbit.io\/v2.0\/forecast\/daily?key=${WEATHERBIT_API_KEY}`;
 
-let listOfTrips = [];
-let currentlySelectedTrip = null;
-let currentlySelectedIndex = null;
+let listOfTrips = [];              //List of scheduled trips
+let currentlySelectedTrip = null;  //Trip that is currently being displayed on the webpage
+let currentlySelectedIndex = null; //Index of the currently selected trip
 
 /* Create an application instance and set the middleware. */
 const app = express();
@@ -39,9 +41,23 @@ const server = app.listen(port, function() {
   console.log('Travel App is running on http://localhost:8081');
 });
 
+/*
+ * Purpose: This function retrieves data from an API.
+ * This function makes an HTTP POST request to an API, 
+ * and returns the data to the client.
+ *
+ * Parameters:
+ * - givenBaseURL: A string containing the base URL for an API request.
+ * - givenQuery: A string containing the query for the API request.
+ *
+ */
 async function getAPIData(givenBaseURL, givenQuery) {
-  const apiResponse = await fetch(givenBaseURL + givenQuery);
+  const apiResponse = await fetch(givenBaseURL + givenQuery); //Fetch data from the specified API.
 
+  /* If the call to the API was successful,
+   * return the data as a Javascript object.
+   * Otherwise, return an error;
+   */
   try {
     const responseData = await apiResponse.json();
     return responseData;
@@ -51,18 +67,26 @@ async function getAPIData(givenBaseURL, givenQuery) {
   }
 }
 
+/* DELETE route for client requests that need to delete one or more trip activities */
 app.delete('/tripActivities', function(req, res) {
-  const selectedData = req.body.selectedData;
+  const selectedData = req.body.selectedData; //Data selected for deletion
+  const activities = currentlySelectedTrip.activities; //List of activities for the currently selected trip
+
+  /* If there is no currently selected trip, notify the client. */
   if(currentlySelectedTrip === null) {
     res.status = 200;
     res.json({message: 'There is no trip that is currently displayed.'});
   }
+  /* Otherwise, delete one or more activities. */
   else {
+
+    /* If the client requested to delete all activities, clear the activities list. */
     if(selectedData === 'all') {
       currentlySelectedTrip.activities = [];
+      listOfTrips[currentlySelectedIndex].activities = [];
     }
-    else {
-      const activities = currentlySelectedTrip.activities;
+    /* Otherwise, delete the specified activities. */
+    else { 
       let i = 0;
       let j = 0;
       for(i = 0; i < selectedData.length; i++) {
@@ -74,36 +98,56 @@ app.delete('/tripActivities', function(req, res) {
         }
       }
     }
+    currentlySelectedTrip.activities = activities;
+    listOfTrips[currentlySelectedIndex].activities = activities;
     res.status = 200;
     res.json({message: 'The data was deleted.'});
   }
 });
 
+/* POST route for client requests that want to add an activity to the currently selected trip's activities list. */
 app.post('/tripActivities', function(req, res) {
+
+  /* If no trip is currently being display by the webpage, notify the client. */
   if(currentlySelectedTrip === null) {
     res.status = 200;
     res.json({message: 'There is no trip that is currently displayed.'});
   }
+  /* Otherwise, add the activity to the currently selected trip's list of activities and return the list. */
   else {
     currentlySelectedTrip.activities.push(req.body.givenActivity);
+    listOfTrips[currentlySelectedIndex].activities.push(req.body.givenActivity);
     res.status = 200;
     res.json({message: "The activities list was updated", listOfActivities: currentlySelectedTrip.activities});
   }
 });
 
+/* POST route for client requests that need to get the geographic coordinates for a given location */
 app.post('/geographicCoordinates', function(req, res) {
-  const cityComponents = req.body.city.split(/ +/);
-  const givenQuery = `&name_equals=${cityComponents.join('+')}`;
+  const cityComponents = req.body.city.split(/ +/); //Split the city string if it contains spaces, and then concatenate the parts with '+'.
+  const givenQuery = `&name_equals=${cityComponents.join('+')}`; //Set the query to look for the city.
+
+  /* Get the geographic coordinates using the GeoNames API. */
   getAPIData(geonamesBaseURL, givenQuery).then(function(data) {
+
+    /* Notify the user if no data could be found for the given city. */
     if(data.totalResultsCount === 0) {
       res.status = 200;
       res.json({message: 'The given city was not found.'});
     }
+
+    /* Variables to indicate if the specified city, administrative division,
+     * and country were found.
+     */
     let cityFound = false;
     let adminDivFound = false;
     let countryFound = false;
-    let resultFound = [];
+    let resultFound = []; //Array to contain data of the found location
     
+    /* Break up the strings for the city, administrative division,
+     * and country. Then, make a regular expression for each item.
+     * The regular expression will be used to check the data.
+     */
     const cityStringPattern = cityComponents.join(',* ');
     const cityRegExp = new RegExp(cityStringPattern, 'i');
     const adminDivComponents = req.body.adminDiv.split(/ +/);
@@ -113,10 +157,20 @@ app.post('/geographicCoordinates', function(req, res) {
     const countryStringPattern = countryComponents.join(',* ');
     const countryRegExp = new RegExp(countryStringPattern, 'i');
 
+    /* Look through the list of geographic data results. */
     for(let geoname of data.geonames) {
+      /* Variables to indicate if the data matches the
+       * city, administrative division, and country
+       * specified by the client.
+       */
       let cityMatch = false;
       let adminDivMatch = false;
       let countryMatch = false;
+      
+      /* Check if the information in the data matches the
+       * city, administrative divison, and country that
+       * are specified by the client.
+       */
       if(geoname.name.match(cityRegExp)) {
         cityMatch = true;
 	cityFound = true;
@@ -129,12 +183,20 @@ app.post('/geographicCoordinates', function(req, res) {
         countryMatch = true;
 	countryFound = true;
       }
+
+      /* Check if all three user specifications were met
+       * (i.e. the specified location was found).
+       */
       if(cityMatch && (adminDivMatch && countryMatch)) {
         resultFound.push(geoname);
 	break;
       }
     }
     res.status = 200;
+    
+    /* If the location was not found, notify the user
+     * which portion of the specified location was not found.
+     */
     if(resultFound.length === 0) {
       if(!cityFound) {
 	res.json({message: 'The given city was not found.'});
@@ -149,6 +211,7 @@ app.post('/geographicCoordinates', function(req, res) {
         res.json({message: 'The given location was not found.'});
       }
     }
+    /* Otherwise, send the geographic data to the client. */
     else {
       res.json({
         message: 'The given location has been located.',
@@ -161,6 +224,9 @@ app.post('/geographicCoordinates', function(req, res) {
 	}
       });
     }
+  /* Catch any errors that occur while attempting to call the GeoNames API,
+   * and notify the client.
+   */
   }).catch(function(error) {
     console.log(`Error: ${error}`);
     res.status = 500;
@@ -168,18 +234,27 @@ app.post('/geographicCoordinates', function(req, res) {
   });
 });
 
+/* DELETE route for client request that want to delete one or more trips. */
 app.delete('/listOfTrips', function(req, res) {
-  const tripsToDelete = req.body.deleteTrips;
+  const tripsToDelete = req.body.deleteTrips; //List of trips to delete
+
+  /* If the user wants to delete all scheduled trips, clear the list of trips. */
   if(tripsToDelete === 'all') {
     listOfTrips = [];
+    currentlySeletedIndex = null;
     currentlySelectedTrip = null;
   }
+  /* Otherwise, delete the specified trips from the list of trips. */
   else {
     let i = 0;
     let j = 0;
     for(i = 0; i < tripsToDelete.length; i++) {
       for(j = 0; j < listOfTrips.length; j++) {
+        /* If a match is found for the specified trip, delete it from the list of trips. */
         if(((tripsToDelete[i].title === listOfTrips[j].title) && (tripsToDelete[i].city === listOfTrips[j].city)) && ((tripsToDelete[i].startDate === listOfTrips[j].startDate) && (tripsToDelete[i].endDate === listOfTrips[j].endDate))) {
+	  /* If a trip is currently being displayed on the webpage, check if the trip that is currently going
+	   * to be deleted is the currently selected trip. If so, update the variables for the currently selected trip.
+	   */
 	  if(currentlySelectedTrip !== null) {
 	    if(((currentlySelectedTrip.title === listOfTrips[j].title) && (currentlySelectedTrip.city === listOfTrips[j].city)) && ((currentlySelectedTrip.startDate === listOfTrips[j].startDate) && (currentlySelectedTrip.endDate === listOfTrips[j].endDate))) {
 	      currentlySelectedTrip = null;
@@ -196,14 +271,21 @@ app.delete('/listOfTrips', function(req, res) {
   res.json({message: 'The data was deleted'});
 });
 
+/* POST route for client requests that want to add a trip to the list of trips, and/or retrieve the list of trips */
 app.post('/listOfTrips', function(req, res) {
+  /* If a trip was given, determine whether to add the trip and/or send the list of trips. */ 
   if(req.body.someTrip !== null) { 
     currentlySelectedTrip = req.body.someTrip;
+
+    /* If the client wants to insert a trip, add the specified trip to the list of trips. */
     if(req.body.method === 'insert') { 
       listOfTrips.push(req.body.someTrip);
       listOfTrips = tripsIterativeMergeSort(listOfTrips);
     }
+
+    /* Find the specified trip in the list of trips. */
     for(let i = 0; i < listOfTrips.length; i++) {
+      /* Once the specified trip is found, set the variables for the currently selected trip. */
       if(((currentlySelectedTrip.title === listOfTrips[i].title) && (currentlySelectedTrip.city === listOfTrips[i].city)) && ((currentlySelectedTrip.startDate === listOfTrips[i].startDate) && (currentlySelectedTrip.endDate === listOfTrips[i].endDate))) {
         currentlySelectedIndex = i;
 	currentlySelectedTrip = listOfTrips[i];
@@ -213,17 +295,23 @@ app.post('/listOfTrips', function(req, res) {
     res.status = 200;
     res.json({tripList: listOfTrips, selectedTrip: currentlySelectedTrip});
   }
+  /* Otherwise, just send the list of trips. */
   else {
     res.status = 200;
     res.json({tripList: listOfTrips});
   }
 });
 
+/* POST route for client requests that want to get weather forecasts for a specified location. */
 app.post('/weatherForecast', function(req, res) {
-  const givenQuery = `&lat=${req.body.latitude}&lon=${req.body.longitude}`;
+  const givenQuery = `&lat=${req.body.latitude}&lon=${req.body.longitude}`; //Use the given geographic coordinates for the query.
+  
+  /* Get the forecasts from the Weatherbit Weather API. */
   getAPIData(weatherbit_forecast_baseURL, givenQuery).then(function(weatherData) {
     res.status = 200;
     res.json({message: 'The weather data for the given coordinates has been retrieved.', weatherInfo: weatherData.data});
+
+  /* If any errors occurred while attempting to call the Weatherbit Weather API, notify the client. */
   }).catch(function(error) {
     console.log(`Error: ${error}`);
     res.status = 500;
@@ -231,40 +319,63 @@ app.post('/weatherForecast', function(req, res) {
   });
 });
 
+/* PUT route for any client requests that want to update the weather forecasts for the currently selected trip. */
 app.put('/weatherForecast', function(req, res) {
-  currentlySelectedTrip.weatherForecasts = req.body.newWeatherForecasts;
-  listOfTrips[currentlySelectedIndex].weatherForecasts = req.body.newWeatherForecasts;
+  currentlySelectedTrip.weatherForecasts = req.body.newWeatherForecasts; //Update the forecasts for the currently selected trip.
+  listOfTrips[currentlySelectedIndex].weatherForecasts = req.body.newWeatherForecasts; //Make the same update in the list of trips.
   res.status = 200;
   res.json({message: 'The weather forecasts for the currently selected trip have been updated.'});
 });
 
+/* POST route for any client requests that want to get a photograph of a specified location. */
 app.post('/pixabayImages', function(req, res) {
-  let givenQuery = '';
+  let givenQuery = ''; //Query to be used when calling the Pixabay API
+
+  /* If the client specified an image's ID, use that ID to find an image. */
   if(req.body.id !== '') {
     givenQuery = `&id=${req.body.id}`;
+    /* Call the Pixabay API, using the image ID to find a specific image. */
     getAPIData(pixabayBaseURL, givenQuery).then(function(retrievedImage) {
-      const imageData = {largeImageURL: retrievedImage.hits[0].largeImageURL};
+      const imageData = {largeImageURL: retrievedImage.hits[0].largeImageURL}; //Data to be returned to the client.
       res.status = 200;
       res.json({message: 'The requested image was retrieved.', imageInfo: imageData, imageID: req.body.id});
+
+    /* If any errors occurred while calling the Pixabay API, notify the client. */
     }).catch(function(error) {
       console.log(`Error: ${error}`);
       res.status = 500;
       res.json({message: 'An error occurred on the server while processing the data.'});
     });
   }
+  /* Otherwise, use the geographic data to find an image for the desired location. */
   else {
+    /* Variables to be used in the image query */
     const photoType = req.body.photoType;
     const city = req.body.city;
     const adminDiv = req.body.adminDiv;
     const country = req.body.country;
+
+    /* First find any images that correspond to the specified city, administrative division, and country. */
     givenQuery = `&category=${photoType}&q=${city},${adminDiv},${country}`.normalize("NFD").replace(/\p{Diacritic}/gu, "");
     getAPIData(pixabayBaseURL, givenQuery).then(function(retrievedImages1) {
+
+      /* Second, find any images that correspond to the specified city and administrative division. */
       givenQuery = `&category=${photoType}&q=${city},${adminDiv}`.normalize("NFD").replace(/\p{Diacritic}/gu, "");
       getAPIData(pixabayBaseURL, givenQuery).then(function(retrievedImages2) {
+
+        /* Third, find any images that correspond to the specified country. */
         givenQuery = `&category=${photoType}&q=${country}`.normalize("NFD").replace(/\p{Diacritic}/gu, "");
         getAPIData(pixabayBaseURL, givenQuery).then(function(retrievedImages3) {
+
+	  /* If any images were found that correspond to the specified city, administrative division,
+	   * and city, send an image to the user.
+	   */
 	  if(retrievedImages1.hits.length !== 0) {
-            if(retrievedImages1.hits.length > 1) {
+            /* If more than one image was found, send the second image
+	     * instead of the first image. This will take advantage
+	     * of the variety of results.
+	     */
+	    if(retrievedImages1.hits.length > 1) {
               res.status = 200;
               res.json({
                 message: 'An image was found for the given location.', 
@@ -275,6 +386,7 @@ app.post('/pixabayImages', function(req, res) {
                 userID: retrievedImages1.hits[1].user_id
               });
             }
+	    /* Otherwise, send the only result. */
             else {
               res.status = 200;
               res.json({
@@ -287,8 +399,17 @@ app.post('/pixabayImages', function(req, res) {
               });
             }
           }
+	  /* Otherwise, see if any images were found that correspond
+	   * to the given city and administrative division. If so,
+	   * send an image to the client.
+	   */
           else if(retrievedImages2.hits.length !== 0) {
-            if(retrievedImages2.hits.length > 1) {
+            /* If more than one image was found for the given city and
+	     * administrative division, send the second result.
+	     * this will take advantage of the variety of the results.
+	     * Otherwise, send the only result.
+	     */
+	    if(retrievedImages2.hits.length > 1) {
               res.status = 200;
               res.json({
                 message: 'An image was found for the given location.', 
@@ -311,8 +432,16 @@ app.post('/pixabayImages', function(req, res) {
               });
             }
           }
+	  /* If no images were found for the specified city,
+	   * try to instead to send an image of the corresponding
+	   * country. If so, select an image to send.
+	   */
 	  else if(retrievedImages3.hits.length !== 0) {
-            if(retrievedImages3.hits.length > 1) {
+            /* If more than one image was found, send the second
+	     * image. This will take advantage of the variety of
+	     * the results. Otherwise, send the only result.
+	     */
+	    if(retrievedImages3.hits.length > 1) {
               res.status = 200;
               res.json({
                 message: 'An image was found for the given location.', 
@@ -335,6 +464,10 @@ app.post('/pixabayImages', function(req, res) {
               });
             }
           }
+	  /* Otherwise, notify the client that no images could
+	   * be found for the specified city or for the corresponding
+	   * country.
+	   */
 	  else {
 	    res.status = 200;
             res.json({
@@ -346,16 +479,29 @@ app.post('/pixabayImages', function(req, res) {
               userID: null
             });
           }
+
+	/* Catch any errors that occurred while
+	 * finding images for the specified country,
+	 * and notify the client.
+	 */
 	}).catch(function(error) {
 	  console.log(`Error Name: ${error.name}, Error Message: ${error.message}`);
           res.status = 500;
           res.json({message: 'An error occurred on the server while processing the data.'});
         });
+      /* Catch any errors that occurred while
+       * finding images for the specified city and
+       * administrative division, and notify the client.
+       */
       }).catch(function(error) {
         console.log(`Error Name: ${error.name}, Error Message: ${error.message}`);
         res.status = 500;
         res.json({message: 'An error occurred on the server while processing the data.'});
       });
+    /* Catch any errors that occurred while
+     * finding images for the specified city and
+     * administrative division, and notify the client.
+     */
     }).catch(function(error) {
       console.log(`Error Name: ${error.name}, Error Message: ${error.message}`);
       res.status = 500;
--- src/server/sortTrips.js
+++ src/server/sortTrips.js
@@ -1,10 +1,30 @@
+/*
+ * Purpose: This function serves as an
+ * iterative merge sort for the trips.
+ * 
+ * Parameters:
+ * - tripList: A list of trips
+ *
+ * Return Value: A list of trips sorted by earliest start
+ * date (smallest countdown), by earliest end date, and
+ * by title.
+ *
+ * Note: This iterative merge sort
+ * is based on the iterative merge sort
+ * presented in the following webpage:
+ * https://www.geeksforgeeks.org/iterative-merge-sort/
+ *
+ */
 function tripsIterativeMergeSort(tripList) {
-  let n = tripList.length;
-  let mergedArray = tripList;
+  let n = tripList.length; //Size of the trip list.
+  let mergedArray = tripList; //Array that will store the sorted list.
 
-  let subArraySize = 0;
-  let leftArrayStart = 0;
+  let subArraySize = 0; //Size of each partial list that will be individually sorted.
+  let leftArrayStart = 0; //Starting index for the leftmost partial list.
 
+  /* Split the main array into approximately equal parts, individually sort those parts,
+   * and merge the parts.
+   */
   for(subArraySize = 1; subArraySize <= (n-1); subArraySize *= 2) {
     for(leftArrayStart = 0; leftArrayStart < (n - 1); leftArrayStart += 2*subArraySize) {
       let midPoint = Math.min(leftArrayStart + subArraySize - 1, n - 1);
@@ -15,16 +35,43 @@ function tripsIterativeMergeSort(tripList) {
   return mergedArray;
 }
 
+/*
+ * Purpose: This function serves as the
+ * merge portion for an iterative merge
+ * sort, which will be used on a list of trips.
+ * 
+ * Parameters:
+ * - mergedArray: Array that contains the trips to sort
+ * - leftArrayStart: Starting index for the left partial array.
+ * - midPoint: Middle point that seperates the two partial arrays.
+ * - rightArrayEnd: Final index for the right partial array.
+ *
+ * Return Value: A sorted combination of the two partial arrays.
+ * This will be a list of trips sorted by earliest start
+ * date (smallest countdown), by earliest end date, and
+ * by title.
+ *
+ * Note: This merge function
+ * is based on the iterative merge sort
+ * presented in the following webpage:
+ * https://www.geeksforgeeks.org/iterative-merge-sort/
+ *
+ */
 function mergeTripArrays(mergedArray, leftArrayStart, midPoint, rightArrayEnd) {
+  /* Variables for going through the partial arrays and merged array. */
   let i = 0;
   let j = 0;
   let k = 0;
   let firstArraySize = midPoint - leftArrayStart + 1
   let secondArraySize = rightArrayEnd - midPoint;
 
+  /* The two partial arrays */
   let leftArray = [];
   let rightArray = [];
 
+  /* Put the array elements for each partial array
+   * into their corresponding array.
+   */
   for(i = 0; i < firstArraySize; i++) {
     leftArray[i] = mergedArray[leftArrayStart + i];
   }
@@ -32,12 +79,23 @@ function mergeTripArrays(mergedArray, leftArrayStart, midPoint, rightArrayEnd) {
     rightArray[j] = mergedArray[midPoint + 1 + j];
   }
 
+  /* Set the variables to begin merging the arrays. */
   i = 0;
   j = 0;
   k = leftArrayStart;
+
+  /* Compare the trips from both partial arrays while
+   * they both have elements left.
+   */
   while((i < firstArraySize) && (j < secondArraySize)) {
+    /* Create Date objects to easily compare different end dates. */
     let firstEndDate = new Date(leftArray[i].endDate);
     let secondEndDate = new Date(rightArray[j].endDate);
+
+    /* Compare the countdown values for each trip. If one is
+     * less that the other, insert the appropriate trip
+     * into the merged array.
+     */
     if(leftArray[i].countdown < rightArray[j].countdown) {
       mergedArray[k] = leftArray[i];
       i++;
@@ -48,7 +106,15 @@ function mergeTripArrays(mergedArray, leftArrayStart, midPoint, rightArrayEnd) {
       j++;
       k++;
     }
+
+    /* If the two trips had the same countdown value,
+     * compare them using their end dates or titles.
+     */
     else {
+      /* If one trip's end date is later than the other trip's
+       * end date, insert the appropriate trip into the merged
+       * array.
+       */
       if(firstEndDate.getTime() < secondEndDate.getTime()) {
         mergedArray[k] = leftArray[i];
         i++;
@@ -59,8 +125,17 @@ function mergeTripArrays(mergedArray, leftArrayStart, midPoint, rightArrayEnd) {
 	j++;
 	k++;
       }
+
+      /* If both trips have the values for their countdown and
+       * end date, compare their titles.
+       */
       else {
-        if(leftArray[i].title < rightArray[j].title) {
+        /* If one trip's title has a value less that is less
+	 * than the value of the other trip's title (i.e. one
+	 * title goes ahead of the other when sorted alphabetically),
+	 * insert the appropriate trip into the merged array.
+	 */
+	if(leftArray[i].title < rightArray[j].title) {
 	  mergedArray[k] = leftArray[i];
 	  i++;
 	  k++;
@@ -70,6 +145,10 @@ function mergeTripArrays(mergedArray, leftArrayStart, midPoint, rightArrayEnd) {
           j++;
           k++;
         }
+	/* If both trips have the same values for their countdown,
+	 * end date, and title, insert both trips into the merged
+	 * array.
+	 */
         else {
           mergedArray[k] = leftArray[i];
           i++;
@@ -82,6 +161,10 @@ function mergeTripArrays(mergedArray, leftArrayStart, midPoint, rightArrayEnd) {
     }
   }
 
+  /* If there are any trips remaining in either
+   * the left array or the right array, insert
+   * those trips into the merged array.
+   */
   while(i < firstArraySize) {
     mergedArray[k] = leftArray[i];
     i++;
@@ -96,6 +179,7 @@ function mergeTripArrays(mergedArray, leftArrayStart, midPoint, rightArrayEnd) {
   return mergedArray;
 }
 
+/* Export the two functions for implementation and testing. */
 module.exports = {
   mergeTripArrays,
   tripsIterativeMergeSort
